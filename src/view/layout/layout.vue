764bb8d28d8cc54445a7d8e55f619a4460037afb
krank <emosick@qq.com>
2020-11-12 14:12:25 +0800

    增加基础页面

---
+++ src/view/layout/layout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ count }}</h1>
+    <Test @ping="ping" :msg="count"></Test>
+  </div>
+</template>
+
+<script lang="ts">
+import Test from "@/components/test.vue";
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  name: "App",
+  components:{Test},
+  setup() {
+    const count = ref("hello gva");
+    const ping = (pang: string): void => {
+      console.log(pang);
+      count.value = "pang";
+    };
+    return {
+      count,
+      ping
+    }
+  },
+});
+</script>
+
+<style scoped></style>
