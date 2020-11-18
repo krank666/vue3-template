94a006bfdb3adca8933b2a5302ab62bdbbcf5797
krank <emosick@qq.com>
2020-11-18 10:02:01 +0800

    组件封装体验

---
+++ src/components/test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div @click="pang">
+    {{ msg }}
+    <el-button>test</el-button>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, onMounted } from "vue";
+import {test} from "@/api/test"
+export default defineComponent({
+  props: {
+    msg: String,
+  },
+  setup(props, context) {
+    const pang = (): void => {
+      context.emit("ping", "pang");
+      return;
+    }
+    onMounted(() => {
+      // test()
+    })
+    return { pang, msg: props.msg };
+  },
+});
+</script>
